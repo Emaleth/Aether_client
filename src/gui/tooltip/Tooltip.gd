8211b250fb8953da5d46c5cf39e26d9bca92e2f1
Emaleth <emaleth@protonmail.com>
2021-04-29 21:34:42 +0200

    tooltips and portret cut with shader

--- src/gui/tooltip/Tooltip.gd
+++ src/gui/tooltip/Tooltip.gd
@@ -1,23 +1,37 @@
 extends PanelContainer
 
 
-var item_name : String
-var stats : Dictionary = {
-	"hp" : "2",
-	"mp" : "25",
-	"sp" : "23",
-	"fk" : "5"
-}
+var item_name : String 
+var description : String 
+var stats : Dictionary 
 
 onready var stat_row = preload("res://src/gui/tooltip/StatRow.tscn").instance()
 onready var stat_container : Container = $MarginContainer/VBoxContainer/StatContainer
-
+onready var name_label = $MarginContainer/VBoxContainer/Name
+onready var name_separator = $MarginContainer/VBoxContainer/HSeparator
+onready var description_label = $MarginContainer/VBoxContainer/Description
+onready var description_separator = $MarginContainer/VBoxContainer/HSeparator2
+	
 func _ready() -> void:
 	get_stats()
 	
+	
 func get_stats() -> void:
-	for i in stats:
-		var sr = stat_row.duplicate()
-		stat_container.add_child(sr)
-		sr.conf("sdf", "sdf", "sdf")
-		
+	if item_name:
+		name_label.text = item_name
+	else:
+		name_label.hide()
+		name_separator.hide()
+	
+	if description:
+		description_label.text = description
+	else:
+		description_label.hide()
+	
+		description_separator.hide()
+	if stats:
+		for i in stats:
+			var sr = stat_row.duplicate()
+			stat_container.add_child(sr)
+			sr.conf("sdf", "sdf", "sdf")
+
--- src/gui/w_base/portret/Portrait.gd
+++ src/gui/w_base/portret/Portrait.gd
@@ -1,19 +1,24 @@
 extends PanelContainer
 
+var item_name : String = "test"
+var item_description : String = "ldfhgksdhgksdfkgsdkhfg"
+var item_stats : Dictionary = {
+	"hp" : "2",
+	"mp" : "25",
+	"sp" : "23",
+	"fk" : "5"
+}
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+	hint_tooltip = "wierd fuckery"
+	
+	
+func _make_custom_tooltip(for_text):
+	var tooltip = preload("res://src/gui/tooltip/Tooltip.tscn").instance()
+	tooltip.item_name = item_name
+	tooltip.description = item_description
+	tooltip.stats = item_stats
+	return tooltip
 
 
 func _on_Button_pressed() -> void:
