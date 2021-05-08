d69eed182af7456c0971c036e500109b82f64685
Emaleth <emaleth@protonmail.com>
2021-05-08 16:14:53 +0200

    ui

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ### 3d
  - human male model : VideroBoy @ https://opengameart.org/content/low-poly-human-male
  - human female model : VideroBoy @ https://opengameart.org/content/low-poly-human-female
-
+ - elven/fey weapons : Clint Bellanger @ https://opengameart.org/content/elven-weapon-set
 ### textures
 
 ### sound
--- src/gui/w_minimap/MiniMap.gd
+++ src/gui/w_minimap/MiniMap.gd
@@ -11,13 +11,17 @@ func _ready() -> void:
 	conf_compass()
 	
 func conf_compass() -> void:
-	north.rect_position = Vector2(compass.rect_size.x / 2 - 10, 0)
-	east.rect_position = Vector2(compass.rect_size.x - 20, compass.rect_size.y / 2 - 10)
-	south.rect_position = Vector2(compass.rect_size.x / 2 - 10, compass.rect_size.y - 20)
-	west.rect_position = Vector2(0, compass.rect_size.y / 2 - 10)
+	north.rect_position = Vector2(compass.rect_size.x / 2 - 20, 0)
+	east.rect_position = Vector2(compass.rect_size.x - 40, compass.rect_size.y / 2 - 20)
+	south.rect_position = Vector2(compass.rect_size.x / 2 - 20, compass.rect_size.y - 40)
+	west.rect_position = Vector2(0, compass.rect_size.y / 2 - 20)
 
 
 func _on_ViewportContainer_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == 1:
 			Input.action_press("map")
+			
+func _process(delta: float) -> void:
+	for i in compass.get_children():
+		i.rect_rotation = -compass.rect_rotation
