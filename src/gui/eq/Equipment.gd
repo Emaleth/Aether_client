56e0c26bcb5da4bfd3a088c4442fd8463fef3290
Emaleth <emaleth@protonmail.com>
2021-05-16 15:31:57 +0200

    almost done with inventory buttons

--- src/gui/eq/Equipment.gd
+++ src/gui/eq/Equipment.gd
@@ -5,91 +5,91 @@ onready var grid = $MarginContainer/GridContainer
 onready var equipment : Dictionary = {
 	"head" : {
 		"slot" : grid.get_node("Head"),
-		"empty_icon" : preload("res://textures/ui_head_ghost.png")
+		"empty_icon" : preload("res://textures/icons/helmet_icon.png")
 		},
 	"hands" : {
 		"slot" : grid.get_node("Hands"),
-		"empty_icon" : preload("res://textures/ui_hands_ghost.png")
+		"empty_icon" : preload("res://textures/icons/gloves_icon.png")
 		},
 	"feet" : {
 		"slot" : grid.get_node("Feet"),
-		"empty_icon" : preload("res://textures/ui_feet_ghost.png")
+		"empty_icon" : preload("res://textures/icons/feet_icon.png")
 		},
 	"upper_body" : {
 		"slot" : grid.get_node("UpperBody"),
-		"empty_icon" : preload("res://textures/ui_upper_body_ghost.png")
+		"empty_icon" : preload("res://textures/icons/armor_icon.png")
 		},
 	"lower_body" : {
 		"slot" : grid.get_node("LowerBody"),
-		"empty_icon" : preload("res://textures/ui_lower_body_ghost.png")
+		"empty_icon" : preload("res://textures/icons/legs_icon.png")
 		},
 	"cape" : {
 		"slot" : grid.get_node("Cape"),
-		"empty_icon" : preload("res://textures/ui_cape_ghost.png")
+		"empty_icon" : preload("res://textures/icons/cape_icon.png")
 		},
 	"belt" : {
 		"slot" : grid.get_node("Belt"),
-		"empty_icon" : preload("res://textures/ui_belt_ghost.png")
+		"empty_icon" : preload("res://textures/icons/belt_icon.png")
 		},
 	"shoulders" : {
 		"slot" : grid.get_node("Shoulders"),
-		"empty_icon" : preload("res://textures/ui_shoulders_ghost.png")
+		"empty_icon" : preload("res://textures/icons/shoulders_icon.png")
 		},
 	"necklace" : {
 		"slot" : grid.get_node("Necklace"),
-		"empty_icon" : preload("res://textures/ui_necklace_ghost.png")
+		"empty_icon" : preload("res://textures/icons/necklace_icon.png")
 		},
 	"ammunition" : {
 		"slot" : grid.get_node("Ammunition"),
-		"empty_icon" : preload("res://textures/ui_ammunition_ghost.png")
+		"empty_icon" : preload("res://textures/icons/ammunition_icon.png")
 		},
 	"ranged_weapon" : {
 		"slot" : grid.get_node("RangedWeapon"),
-		"empty_icon" : preload("res://textures/ui_ranged_weapon_ghost.png")
+		"empty_icon" : preload("res://textures/icons/ranged_weapon_icon.png")
 		},
 	"ring_1" : {
 		"slot" : grid.get_node("Ring1"),
-		"empty_icon" : preload("res://textures/ui_ring_ghost.png")
+		"empty_icon" : preload("res://textures/icons/ring_icon.png")
 		},
 	"ring_2" : {
 		"slot" : grid.get_node("Ring2"),
-		"empty_icon" : preload("res://textures/ui_ring_ghost.png")
+		"empty_icon" : preload("res://textures/icons/ring_icon.png")
 		},
 	"earring_1" : {
 		"slot" : grid.get_node("Earring1"),
-		"empty_icon" : preload("res://textures/ui_earring_ghost.png")
+		"empty_icon" : preload("res://textures/icons/earrings_icon.png")
 		},
 	"earring_2" : {
 		"slot" : grid.get_node("Earring2"),
-		"empty_icon" : preload("res://textures/ui_earring_ghost.png")
+		"empty_icon" : preload("res://textures/icons/earrings_icon.png")
 		},
 	"main_hand" : {
 		"slot" : grid.get_node("MainHand"),
-		"empty_icon" : preload("res://textures/ui_main_hand_ghost.png")
+		"empty_icon" : preload("res://textures/icons/weapon_icon.png")
 		},
 	"off_hand" : {
 		"slot" : grid.get_node("OffHand"),
-		"empty_icon" : preload("res://textures/ui_off_hand_ghost.png")
+		"empty_icon" : preload("res://textures/icons/shield_icon.png")
 		},
 	"gathering_tools" : {
 		"slot" : grid.get_node("GatheringTools"),
-		"empty_icon" : preload("res://textures/ui_gathering_tools_ghost.png")
+		"empty_icon" : preload("res://textures/icons/tool_icon.png")
 		},
 	"amulet_1" : {
 		"slot" : grid.get_node("Amulet1"),
-		"empty_icon" : preload("res://textures/ui_amulet_ghost.png")
+		"empty_icon" : preload("res://textures/icons/amulet_icon.png")
 		},
 	"amulet_2" : {
 		"slot" : grid.get_node("Amulet2"),
-		"empty_icon" : preload("res://textures/ui_amulet_ghost.png")
+		"empty_icon" : preload("res://textures/icons/amulet_icon.png")
 		},
 	"amulet_3" : {
 		"slot" : grid.get_node("Amulet3"),
-		"empty_icon" : preload("res://textures/ui_amulet_ghost.png")
+		"empty_icon" : preload("res://textures/icons/amulet_icon.png")
 		},
 	"back" : {
 		"slot" : grid.get_node("Back"),
-		"empty_icon" : preload("res://textures/ui_back_ghost.png")
+		"empty_icon" : preload("res://textures/icons/spiky-wing.png")
 		}
 	}
 
--- src/gui/w_base/inventory/Inventory.gd
+++ src/gui/w_base/inventory/Inventory.gd
@@ -3,6 +3,49 @@ extends PanelContainer
 var window_height_in_slots = 5
 var grid_v_separation = 4
 
+onready var buttons = {
+	"everything" : {
+		"button" : $MarginContainer/VBoxContainer/SlotSelector/Everything,
+		"on_icon" : preload("res://textures/icons/amulet_icon.png"), 
+		"off_icon" : null
+		},
+	"weapon" : {
+		"button" : $MarginContainer/VBoxContainer/SlotSelector/Weapon,
+		"on_icon" : null, 
+		"off_icon" : null
+		},
+	"armor" : {
+		"button" : $MarginContainer/VBoxContainer/SlotSelector/Armor,
+		"on_icon" : null, 
+		"off_icon" : null
+		},
+	"jewelery" : {
+		"button" : $MarginContainer/VBoxContainer/SlotSelector/Jewelry,
+		"on_icon" : null, 
+		"off_icon" : null
+		},
+	"ammunition" : {
+		"button" : $MarginContainer/VBoxContainer/SlotSelector/Ammunition,
+		"on_icon" : preload("res://textures/icons/ammunition_icon.png"), 
+		"off_icon" : null
+		},
+	"consumable" : {
+		"button" : $MarginContainer/VBoxContainer/SlotSelector/Consumable,
+		"on_icon" : null, 
+		"off_icon" : null
+		},
+	"tool" : {
+		"button" : $MarginContainer/VBoxContainer/SlotSelector/Tool,
+		"on_icon" : null, 
+		"off_icon" : null
+		},
+	"material" : {
+		"button" : $MarginContainer/VBoxContainer/SlotSelector/Material,
+		"on_icon" : null, 
+		"off_icon" : null
+		}
+	}
+	
 onready var slot_grid = $MarginContainer/VBoxContainer/ScrollContainer/grid
 onready var scroll = $MarginContainer/VBoxContainer/ScrollContainer
 onready var slot_selector = $MarginContainer/VBoxContainer/SlotSelector
@@ -11,7 +54,7 @@ onready var slot_path = preload("res://src/gui/w_base/slot/Slot.tscn")
 
 func _ready() -> void:
 	scroll.rect_min_size.y = ((window_height_in_slots * 40) + ((window_height_in_slots -1) * grid_v_separation))
-	slot_selector.xxx = self
+	connect_button()
 	
 func conf(actor):
 	if slot_grid.get_child_count() < actor.inventory.size():
@@ -30,14 +73,22 @@ func conf(actor):
 func can_drop_data(position: Vector2, data) -> bool:
 	return false
 		
-func show_slots(slot_types : Array = []):
+func show_slot_type(slot_type : String = ""):
 	for slot in slot_grid.get_children():
-		if slot.aactor.get(slot.ttype).get(slot.sslot).item:
-			if DataLoader.item_db.get(slot.aactor.get(slot.ttype).get(slot.sslot).item).TYPE in slot_types:
-				slot.show()
-
-func hide_slots(slot_types : Array = []):
-	for slot in slot_grid.get_children():
-		if slot.aactor.get(slot.ttype).get(slot.sslot).item:
-			if DataLoader.item_db.get(slot.aactor.get(slot.ttype).get(slot.sslot).item).TYPE in slot_types:
-				slot.hide()
+		if slot_type == "everything":
+			slot.show()
+		else:
+			if slot.aactor.get(slot.ttype).get(slot.sslot).item:
+				if DataLoader.item_db.get(slot.aactor.get(slot.ttype).get(slot.sslot).item).TYPE == slot_type:
+					slot.show()
+				else:
+					slot.hide()
+	for i in buttons:
+		if i == slot_type:
+			buttons.get(i).button.icon = buttons.get(i).on_icon
+		else:
+			buttons.get(i).button.icon = buttons.get(i).off_icon
+			
+func connect_button():
+	for i in buttons:
+		buttons.get(i).button.connect("pressed", self, "show_slot_type", [i])
