2691178a39fca95c9969cab6bfc53ca0e372c846
Emaleth <Emaleth@protonmail.com>
2021-07-11 12:21:09 +0200

    quickbar static slot number

--- gui/quick_bar/QuickBar.gd
+++ gui/quick_bar/QuickBar.gd
@@ -1,34 +1,15 @@
 extends PanelContainer
 
-var slot = preload("res://gui/slot/Slot.tscn")
-onready var grid = $GridContainer
-var selected_slot = 0
+onready var h_box = $HBoxContainer
 	
 func conf(quickbar):
-	for old_slot in grid.get_children():
-		grid.remove_child(old_slot)
-		old_slot.queue_free()
+	var slots = [] 
+	for i in h_box.get_children():
+		if not i is VSeparator:
+			slots.append(i)
+	var index = 0
 	for i in quickbar:
-		var new_slot = slot.instance()
-		grid.add_child(new_slot)
-		new_slot.conf("quickbar", quickbar, i)
-		
-	grid.get_child(selected_slot).get_node("Selector").show()
-	
-func _unhandled_input(_event: InputEvent) -> void:
-	
-	if Input.is_action_just_pressed("next_skill"):
-		grid.get_child(selected_slot).get_node("Selector").hide()
-		selected_slot = clamp(selected_slot + 1, 0, grid.get_child_count()-1)
-		grid.get_child(selected_slot).get_node("Selector").show()
-	if Input.is_action_just_pressed("previous_skill"):
-		grid.get_child(selected_slot).get_node("Selector").hide()
-		selected_slot = clamp(selected_slot - 1, 0, grid.get_child_count()-1)
-		grid.get_child(selected_slot).get_node("Selector").show()
+		slots[index].conf("quickbar", quickbar, i)
+		index += 1
 
-	if Input.is_action_just_pressed("primary_action"):
-		grid.get_child(selected_slot)._on_Slot_pressed()
-
-#	if Input.is_action_just_pressed("secondary_action"):
-#		get_selected()
 	
