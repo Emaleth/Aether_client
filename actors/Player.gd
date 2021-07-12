934ea6f6d5b2a6613b3e069c74882b50240de11b
Emaleth <Emaleth@protonmail.com>
2021-07-12 22:35:21 +0200

    walk and sprint states

--- actors/Player.gd
+++ actors/Player.gd
@@ -1,8 +1,11 @@
 extends KinematicBody
 
-enum {IDLE, RUN, JUMP, FALL, DEAD}
+enum {IDLE, WALK, RUN, SPRINT, JUMP, FALL, DEAD}
 # player variables
-var speed = 10
+var walk_speed = 5
+var run_speed = 10
+var air_speed = 7
+var sprint_speed = 15
 var acceleration = 10
 var deceleration = 30
 var jump_force = 20
@@ -16,6 +19,8 @@ var velocity = Vector3.ZERO
 var gravity = Vector3.ZERO
 var movement = Vector3.ZERO
 var jumping = false
+var walking = false
+var sprinting = false
 
 onready var gui = preload("res://gui/GUI.tscn")
 onready var indicator = preload("res://gui/actor_indicator/ActorIndicator.tscn")
@@ -27,6 +32,7 @@ func _ready() -> void:
 	conf()
 	
 func _physics_process(delta: float) -> void:
+	get_input()
 	finite_state_machine(delta, get_direction())
 		
 func conf():
@@ -50,16 +56,21 @@ func finite_state_machine(delta: float, direction) -> void:
 			velocity = velocity.linear_interpolate(Vector3.ZERO, deceleration * delta)
 			gravity = Vector3.ZERO
 			
-			if get_direction() != Vector3.ZERO:
-				state = RUN
 			if jumping == true:
 				state = JUMP
+			if get_direction() != Vector3.ZERO:
+				if sprinting == true:
+					state = SPRINT
+				elif walking == true:
+					state = WALK
+				else:
+					state = RUN
 			if not is_on_floor() and velocity.y < 0:
 				state = FALL
-
-		RUN:
+				
+		WALK:
 			snap = -get_floor_normal()
-			velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
+			velocity = velocity.linear_interpolate(direction * walk_speed, acceleration * delta)
 			gravity = Vector3.ZERO
 			
 			if jumping == true:
@@ -68,24 +79,56 @@ func finite_state_machine(delta: float, direction) -> void:
 				state = FALL
 			if get_direction() == Vector3.ZERO:
 				state = IDLE
-	
+			else:
+				if walking == false:
+					state = RUN
+			
+		RUN:
+			snap = -get_floor_normal()
+			velocity = velocity.linear_interpolate(direction * run_speed, acceleration * delta)
+			gravity = Vector3.ZERO
+			
+			if jumping == true:
+				state = JUMP
+			if sprinting == true:
+				state = SPRINT
+			if walking == true:
+				state = WALK
+			if not is_on_floor():
+				state = FALL
+			if get_direction() == Vector3.ZERO:
+				state = IDLE
+				
+		SPRINT:
+			snap = -get_floor_normal()
+			velocity = velocity.linear_interpolate(direction * sprint_speed, acceleration * delta)
+			gravity = Vector3.ZERO
+			
+			if jumping == true:
+				state = JUMP
+			if get_direction() == Vector3.ZERO:
+				state = IDLE
+			else:
+				if sprinting == false:
+					state = RUN
+			if not is_on_floor():
+				state = FALL
+			
 		JUMP:
 			snap = Vector3.DOWN
-			velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
+			velocity = velocity.linear_interpolate(direction * air_speed, acceleration * delta)
 			gravity += Vector3.DOWN * gravity_force * delta
-			if jumping == true:
+			if is_on_floor():
 				snap = Vector3.ZERO
 				gravity = Vector3.UP * jump_force
-				jumping = false
-			
 			if gravity.y < 0:
 				state = FALL
 			
 		FALL:
 			snap = Vector3.DOWN
-			velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
+			velocity = velocity.linear_interpolate(direction * air_speed, acceleration * delta)
 			gravity += Vector3.DOWN * gravity_force * delta
-			
+			jumping = false
 			if is_on_floor():
 				state = IDLE
 				
@@ -103,10 +146,23 @@ func get_direction():
 	
 	return direction
 	
-func _unhandled_input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+func get_input():
+	if Input.is_action_pressed("jump"):
 		jumping = true
+	else:
+		jumping = false
+
+	if Input.is_action_pressed("walk"):
+		walking = true
+	else:
+		walking = false
 		
+	if Input.is_action_pressed("sprint"):
+		sprinting = true
+	else:
+		sprinting = false
+	
+func _unhandled_input(event: InputEvent) -> void:
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
 			if abs(event.relative.x) > .1: 
