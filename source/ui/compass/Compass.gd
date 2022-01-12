912b667447328849008fa84938cd96a627dc75b4
Emaleth <Emaleth@protonmail.com>
2022-01-12 21:31:42 +0100

    Changes to be committed: 	modified:   source/ui/compass/Compass.gd 	modified:   source/ui/compass/Compass.tscn

--- source/ui/compass/Compass.gd
+++ source/ui/compass/Compass.gd
@@ -8,8 +8,9 @@ func _process(delta: float) -> void:
 	
 	
 func configure(_degrees : float):
-	var normalized_rot = _degrees / 180.0
-	var moved_amount = normalized_rot * direction_strip.rect_size.x 
-	direction_strip.rect_position.x = moved_amount / 2.0
-	direction_strip.rect_position.x = wrapf(direction_strip.rect_position.x / 2, -direction_strip.rect_size.x / 4, direction_strip.rect_size.x / 4)
+	var center = rect_size.x / 2.0
+	var step = direction_strip.rect_size.x / 720.0
+	direction_strip.rect_position.x = (-direction_strip.rect_size.x / 2.0 + center + (step * _degrees)) 
+
+
 
