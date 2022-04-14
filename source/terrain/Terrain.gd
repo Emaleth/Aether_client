9984760b5ead737386fbea06afa939e1f89d72bd
Emaleth <Emaleth@protonmail.com>
2022-04-15 00:04:11 +0200

    Changes to be committed: 	new file:   assets/third_party/PBR/Rock028_1K-PNG.zip 	new file:   assets/third_party/PBR/Rock028_1K-PNG/Rock028_1K-PNG.usda 	new file:   assets/third_party/PBR/Rock028_1K-PNG/Rock028_1K-PNG.usdc 	new file:   assets/third_party/PBR/Rock028_1K-PNG/Rock028_1K_AmbientOcclusion.png 	new file:   assets/third_party/PBR/Rock028_1K-PNG/Rock028_1K_AmbientOcclusion.png.import 	new file:   assets/third_party/PBR/Rock028_1K-PNG/Rock028_1K_Color.png 	new file:   assets/third_party/PBR/Rock028_1K-PNG/Rock028_1K_Color.png.import 	new file:   assets/third_party/PBR/Rock028_1K-PNG/Rock028_1K_Displacement.png 	new file:   assets/third_party/PBR/Rock028_1K-PNG/Rock028_1K_Displacement.png.import 	new file:   assets/third_party/PBR/Rock028_1K-PNG/Rock028_1K_NormalDX.png 	new file:   assets/third_party/PBR/Rock028_1K-PNG/Rock028_1K_NormalDX.png.import 	new file:   assets/third_party/PBR/Rock028_1K-PNG/Rock028_1K_NormalGL.png 	new file:   assets/third_party/PBR/Rock028_1K-PNG/Rock028_1K_NormalGL.png.import 	new file:   assets/third_party/PBR/Rock028_1K-PNG/Rock028_1K_Roughness.png 	new file:   assets/third_party/PBR/Rock028_1K-PNG/Rock028_1K_Roughness.png.import 	new file:   assets/third_party/PBR/Rock028_1K-PNG/Rock028_PREVIEW.jpg 	new file:   assets/third_party/PBR/Rock028_1K-PNG/Rock028_PREVIEW.jpg.import 	modified:   assets/third_party/textures/Terrain001_2K.exr 	modified:   assets/third_party/textures/Terrain001_2K.exr.import 	new file:   assets/third_party/textures/Terrain001_2K.exr~ 	modified:   assets/third_party/textures/Terrain001_2K_sm.exr 	new file:   assets/third_party/textures/Terrain001_2K_sm.exr~ 	new file:   assets/third_party/textures/Terrain004_2K.exr 	new file:   assets/third_party/textures/Terrain004_2K.exr.import 	modified:   assets/third_party/textures/Terrain004_2K.exr~ 	new file:   assets/third_party/textures/Terrain004_2K_sm.exr 	new file:   assets/third_party/textures/Terrain004_2K_sm.exr.import 	new file:   assets/third_party/textures/Terrain004_2K_sm.exr~ 	new file:   assets/third_party/textures/ts0.png~ 	new file:   assets/third_party/textures/ts1.png~ 	modified:   resources/materials/terrain.tres 	modified:   resources/shaders/terrain.tres 	modified:   source/terrain/Terrain.gd 	modified:   source/terrain/Terrain.tscn 	modified:   source/world/World.tscn

--- source/terrain/Terrain.gd
+++ source/terrain/Terrain.gd
@@ -6,111 +6,53 @@ export(bool) var generate := false
 export(int) var max_altitude = 1
 
 export(Image) var heightmap
-export(Texture) var normalmap
+export(Texture) var splatmap
 
-export(Texture) var splatmap_0
-#export(Texture) var splatmap_1
-#export(Texture) var splatmap_2
-#export(Texture) var splatmap_3
+export(float) var rock_slope = 0.65
+export(float) var dirt_slope = 0.6
+export(float) var grass_slope = 0.0
 
-export(int) var red_0_channel_scale = 1
-export(Texture) var red_0_channel_diffuse
-export(Texture) var red_0_channel_normal
-export(Texture) var red_0_channel_roughness
-export(Texture) var red_0_channel_ao
+export(int) var rock_channel_scale = 1
+export(Texture) var rock_channel_diffuse
+export(Texture) var rock_channel_normal
+export(Texture) var rock_channel_roughness
+export(Texture) var rock_channel_ao
 
-export var green_0_channel_scale := 1
-export var green_0_channel_diffuse : Texture
-export var green_0_channel_normal : Texture
-export var green_0_channel_roughness : Texture
-export var green_0_channel_ao : Texture
+export(int) var dirt_channel_scale = 1
+export(Texture) var dirt_channel_diffuse
+export(Texture) var dirt_channel_normal
+export(Texture) var dirt_channel_roughness
+export(Texture) var dirt_channel_ao
 
-export var blue_0_channel_scale := 1
-export var blue_0_channel_diffuse : Texture
-export var blue_0_channel_normal : Texture
-export var blue_0_channel_roughness : Texture
-export var blue_0_channel_ao : Texture
+export(int) var grass_channel_scale = 1
+export(Texture) var grass_channel_diffuse
+export(Texture) var grass_channel_normal
+export(Texture) var grass_channel_roughness
+export(Texture) var grass_channel_ao
 
-export var alpha_0_channel_scale := 1
-export var alpha_0_channel_diffuse : Texture
-export var alpha_0_channel_normal : Texture
-export var alpha_0_channel_roughness : Texture
-export var alpha_0_channel_ao : Texture
+export(int) var red_channel_scale = 1
+export(Texture) var red_channel_diffuse
+export(Texture) var red_channel_normal
+export(Texture) var red_channel_roughness
+export(Texture) var red_channel_ao
 
-#
-#export var red_1_channel_scale := 1
-#export var red_1_channel_diffuse : Texture
-#export var red_1_channel_normal : Texture
-#export var red_1_channel_roughness : Texture
-#export var red_1_channel_ao : Texture
-#
-#export var green_1_channel_scale := 1
-#export var green_1_channel_diffuse : Texture
-#export var green_1_channel_normal : Texture
-#export var green_1_channel_roughness : Texture
-#export var green_1_channel_ao : Texture
-#
-#export var blue_1_channel_scale := 1
-#export var blue_1_channel_diffuse : Texture
-#export var blue_1_channel_normal : Texture
-#export var blue_1_channel_roughness : Texture
-#export var blue_1_channel_ao : Texture
-#
-#export var alpha_1_channel_scale := 1
-#export var alpha_1_channel_diffuse : Texture
-#export var alpha_1_channel_normal : Texture
-#export var alpha_1_channel_roughness : Texture
-#export var alpha_1_channel_ao : Texture
-#
-#
-#export var red_2_channel_scale := 1
-#export var red_2_channel_diffuse : Texture
-#export var red_2_channel_normal : Texture
-#export var red_2_channel_roughness : Texture
-#export var red_2_channel_ao : Texture
-#
-#export var green_2_channel_scale := 1
-#export var green_2_channel_diffuse : Texture
-#export var green_2_channel_normal : Texture
-#export var green_2_channel_roughness : Texture
-#export var green_2_channel_ao : Texture
-#
-#export var blue_2_channel_scale := 1
-#export var blue_2_channel_diffuse : Texture
-#export var blue_2_channel_normal : Texture
-#export var blue_2_channel_roughness : Texture
-#export var blue_2_channel_ao : Texture
-#
-#export var alpha_2_channel_scale := 1
-#export var alpha_2_channel_diffuse : Texture
-#export var alpha_2_channel_normal : Texture
-#export var alpha_2_channel_roughness : Texture
-#export var alpha_2_channel_ao : Texture
-#
-#
-#export var red_3_channel_scale := 1
-#export var red_3_channel_diffuse : Texture
-#export var red_3_channel_normal : Texture
-#export var red_3_channel_roughness : Texture
-#export var red_3_channel_ao : Texture
-#
-#export var green_3_channel_scale := 1
-#export var green_3_channel_diffuse : Texture
-#export var green_3_channel_normal : Texture
-#export var green_3_channel_roughness : Texture
-#export var green_3_channel_ao : Texture
-#
-#export var blue_3_channel_scale := 1
-#export var blue_3_channel_diffuse : Texture
-#export var blue_3_channel_normal : Texture
-#export var blue_3_channel_roughness : Texture
-#export var blue_3_channel_ao : Texture
-#
-#export var alpha_3_channel_scale := 1
-#export var alpha_3_channel_diffuse : Texture
-#export var alpha_3_channel_normal : Texture
-#export var alpha_3_channel_roughness : Texture
-#export var alpha_3_channel_ao : Texture
+export(int) var green_channel_scale = 1
+export(Texture) var green_channel_diffuse
+export(Texture) var green_channel_normal
+export(Texture) var green_channel_roughness
+export(Texture) var green_channel_ao
+
+export(int) var blue_channel_scale = 1
+export(Texture) var blue_channel_diffuse
+export(Texture) var blue_channel_normal
+export(Texture) var blue_channel_roughness
+export(Texture) var blue_channel_ao
+
+export(int) var alpha_channel_scale = 1
+export(Texture) var alpha_channel_diffuse
+export(Texture) var alpha_channel_normal
+export(Texture) var alpha_channel_roughness
+export(Texture) var alpha_channel_ao
 
 var map_size : Vector2
 var collision_shape : CollisionShape
@@ -152,130 +94,53 @@ func configure_shader():
 	
 	terrain_shader.set_shader_param("heightmap", generate_heightmap_texture())
 	terrain_shader.set_shader_param("normalmap", generate_normalmap_texture())
-	if splatmap_0:
-		terrain_shader.set_shader_param("splatmap_0_enabled", true)
-		terrain_shader.set_shader_param("splatmap_0", splatmap_0)
-#	if splatmap_1:
-#		terrain_shader.set_shader_param("splatmap_1_enabled", true)
-#		terrain_shader.set_shader_param("splatmap_1", splatmap_1)
-#	if splatmap_2:
-#		terrain_shader.set_shader_param("splatmap_2_enabled", true)
-#		terrain_shader.set_shader_param("splatmap_2", splatmap_2)
-#	if splatmap_3:
-#		terrain_shader.set_shader_param("splatmap_3_enabled", true)
-#		terrain_shader.set_shader_param("splatmap_3", splatmap_3)
+	terrain_shader.set_shader_param("splatmap", splatmap)
+	
+	terrain_shader.set_shader_param("rock_slope", rock_slope)
+	terrain_shader.set_shader_param("dirt_slope", dirt_slope)
+	terrain_shader.set_shader_param("grass_slope", grass_slope)
 
-	if red_0_channel_diffuse:
-		terrain_shader.set_shader_param("red_0_enabled", true)
-	else:
-		terrain_shader.set_shader_param("red_0_enabled", false)
-	terrain_shader.set_shader_param("red_0_scale", red_0_channel_scale)
-	terrain_shader.set_shader_param("red_0_diffuse", red_0_channel_diffuse)
-	terrain_shader.set_shader_param("red_0_normal", red_0_channel_normal)
-	terrain_shader.set_shader_param("red_0_roughness", red_0_channel_roughness)
-	terrain_shader.set_shader_param("red_0_ao", red_0_channel_ao)
+	terrain_shader.set_shader_param("rock_scale", rock_channel_scale)
+	terrain_shader.set_shader_param("rock_diffuse", rock_channel_diffuse)
+	terrain_shader.set_shader_param("rock_normal", rock_channel_normal)
+	terrain_shader.set_shader_param("rock_roughness", rock_channel_roughness)
+	terrain_shader.set_shader_param("rock_ao", rock_channel_ao)
+	
+	terrain_shader.set_shader_param("dirt_scale", dirt_channel_scale)
+	terrain_shader.set_shader_param("dirt_diffuse", dirt_channel_diffuse)
+	terrain_shader.set_shader_param("dirt_normal", dirt_channel_normal)
+	terrain_shader.set_shader_param("dirt_roughness", dirt_channel_roughness)
+	terrain_shader.set_shader_param("dirt_ao", dirt_channel_ao)
+	
+	terrain_shader.set_shader_param("grass_scale", grass_channel_scale)
+	terrain_shader.set_shader_param("grass_diffuse", grass_channel_diffuse)
+	terrain_shader.set_shader_param("grass_normal", grass_channel_normal)
+	terrain_shader.set_shader_param("grass_roughness", grass_channel_roughness)
+	terrain_shader.set_shader_param("grass_ao", grass_channel_ao)
 
-	if green_0_channel_diffuse:
-		terrain_shader.set_shader_param("green_0_enabled", true)
-	else:
-		terrain_shader.set_shader_param("green_0_enabled", false)
-	terrain_shader.set_shader_param("green_0_scale", green_0_channel_scale)
-	terrain_shader.set_shader_param("green_0_diffuse", green_0_channel_diffuse)
-	terrain_shader.set_shader_param("green_0_normal", green_0_channel_normal)
-	terrain_shader.set_shader_param("green_0_roughness", green_0_channel_roughness)
-	terrain_shader.set_shader_param("green_0_ao", green_0_channel_ao)
+	terrain_shader.set_shader_param("red_scale", red_channel_scale)
+	terrain_shader.set_shader_param("red_diffuse", red_channel_diffuse)
+	terrain_shader.set_shader_param("red_normal", red_channel_normal)
+	terrain_shader.set_shader_param("red_roughness", red_channel_roughness)
+	terrain_shader.set_shader_param("red_ao", red_channel_ao)
 
-	if blue_0_channel_diffuse:
-		terrain_shader.set_shader_param("blue_0_enabled", true)
-	else:
-		terrain_shader.set_shader_param("blue_0_enabled", false)
-	terrain_shader.set_shader_param("blue_0_scale", blue_0_channel_scale)
-	terrain_shader.set_shader_param("blue_0_diffuse", blue_0_channel_diffuse)
-	terrain_shader.set_shader_param("blue_0_normal", blue_0_channel_normal)
-	terrain_shader.set_shader_param("blue_0_roughness", blue_0_channel_roughness)
-	terrain_shader.set_shader_param("blue_0_ao", blue_0_channel_ao)
+	terrain_shader.set_shader_param("green_scale", green_channel_scale)
+	terrain_shader.set_shader_param("green_diffuse", green_channel_diffuse)
+	terrain_shader.set_shader_param("green_normal", green_channel_normal)
+	terrain_shader.set_shader_param("green_roughness", green_channel_roughness)
+	terrain_shader.set_shader_param("green_ao", green_channel_ao)
 
-	if alpha_0_channel_diffuse:
-		terrain_shader.set_shader_param("alpha_0_enabled", true)
-	else:
-		terrain_shader.set_shader_param("alpha_0_enabled", false)
-	terrain_shader.set_shader_param("alpha_0_scale", alpha_0_channel_scale)
-	terrain_shader.set_shader_param("alpha_0_diffuse", alpha_0_channel_diffuse)
-	terrain_shader.set_shader_param("alpha_0_normal", alpha_0_channel_normal)
-	terrain_shader.set_shader_param("alpha_0_roughness", alpha_0_channel_roughness)
-	terrain_shader.set_shader_param("alpha_0_ao", alpha_0_channel_ao)
+	terrain_shader.set_shader_param("blue_scale", blue_channel_scale)
+	terrain_shader.set_shader_param("blue_diffuse", blue_channel_diffuse)
+	terrain_shader.set_shader_param("blue_normal", blue_channel_normal)
+	terrain_shader.set_shader_param("blue_roughness", blue_channel_roughness)
+	terrain_shader.set_shader_param("blue_ao", blue_channel_ao)
 
-#	terrain_shader.set_shader_param("red_1_scale", red_1_channel_scale)
-#	terrain_shader.set_shader_param("red_1_diffuse", red_1_channel_diffuse)
-#	terrain_shader.set_shader_param("red_1_normal", red_1_channel_normal)
-#	terrain_shader.set_shader_param("red_1_roughness", red_1_channel_roughness)
-#	terrain_shader.set_shader_param("red_1_ao", red_1_channel_ao)
-#
-#	terrain_shader.set_shader_param("green_1_scale", green_1_channel_scale)
-#	terrain_shader.set_shader_param("green_1_diffuse", green_1_channel_diffuse)
-#	terrain_shader.set_shader_param("green_1_normal", green_1_channel_normal)
-#	terrain_shader.set_shader_param("green_1_roughness", green_1_channel_roughness)
-#	terrain_shader.set_shader_param("green_1_ao", green_1_channel_ao)
-#
-#	terrain_shader.set_shader_param("blue_1_scale", blue_1_channel_scale)
-#	terrain_shader.set_shader_param("blue_1_diffuse", blue_1_channel_diffuse)
-#	terrain_shader.set_shader_param("blue_1_normal", blue_1_channel_normal)
-#	terrain_shader.set_shader_param("blue_1_roughness", blue_1_channel_roughness)
-#	terrain_shader.set_shader_param("blue_1_ao", blue_1_channel_ao)
-#
-#	terrain_shader.set_shader_param("alpha_1_scale", alpha_1_channel_scale)
-#	terrain_shader.set_shader_param("alpha_1_diffuse", alpha_1_channel_diffuse)
-#	terrain_shader.set_shader_param("alpha_1_normal", alpha_1_channel_normal)
-#	terrain_shader.set_shader_param("alpha_1_roughness", alpha_1_channel_roughness)
-#	terrain_shader.set_shader_param("alpha_1_ao", alpha_1_channel_ao)
-#
-#	terrain_shader.set_shader_param("red_2_scale", red_2_channel_scale)
-#	terrain_shader.set_shader_param("red_2_diffuse", red_2_channel_diffuse)
-#	terrain_shader.set_shader_param("red_2_normal", red_2_channel_normal)
-#	terrain_shader.set_shader_param("red_2_roughness", red_2_channel_roughness)
-#	terrain_shader.set_shader_param("red_2_ao", red_2_channel_ao)
-#
-#	terrain_shader.set_shader_param("green_2_scale", green_2_channel_scale)
-#	terrain_shader.set_shader_param("green_2_diffuse", green_2_channel_diffuse)
-#	terrain_shader.set_shader_param("green_2_normal", green_2_channel_normal)
-#	terrain_shader.set_shader_param("green_2_roughness", green_2_channel_roughness)
-#	terrain_shader.set_shader_param("green_2_ao", green_2_channel_ao)
-#
-#	terrain_shader.set_shader_param("blue_2_scale", blue_2_channel_scale)
-#	terrain_shader.set_shader_param("blue_2_diffuse", blue_2_channel_diffuse)
-#	terrain_shader.set_shader_param("blue_2_normal", blue_2_channel_normal)
-#	terrain_shader.set_shader_param("blue_2_roughness", blue_2_channel_roughness)
-#	terrain_shader.set_shader_param("blue_2_ao", blue_2_channel_ao)
-#
-#	terrain_shader.set_shader_param("alpha_2_scale", alpha_2_channel_scale)
-#	terrain_shader.set_shader_param("alpha_2_diffuse", alpha_2_channel_diffuse)
-#	terrain_shader.set_shader_param("alpha_2_normal", alpha_2_channel_normal)
-#	terrain_shader.set_shader_param("alpha_2_roughness", alpha_2_channel_roughness)
-#	terrain_shader.set_shader_param("alpha_2_ao", alpha_2_channel_ao)
-#
-#	terrain_shader.set_shader_param("red_3_scale", red_3_channel_scale)
-#	terrain_shader.set_shader_param("red_3_diffuse", red_3_channel_diffuse)
-#	terrain_shader.set_shader_param("red_3_normal", red_3_channel_normal)
-#	terrain_shader.set_shader_param("red_3_roughness", red_3_channel_roughness)
-#	terrain_shader.set_shader_param("red_3_ao", red_3_channel_ao)
-#
-#	terrain_shader.set_shader_param("green_3_scale", green_3_channel_scale)
-#	terrain_shader.set_shader_param("green_3_diffuse", green_3_channel_diffuse)
-#	terrain_shader.set_shader_param("green_3_normal", green_3_channel_normal)
-#	terrain_shader.set_shader_param("green_3_roughness", green_3_channel_roughness)
-#	terrain_shader.set_shader_param("green_3_ao", green_3_channel_ao)
-#
-#	terrain_shader.set_shader_param("blue_3_scale", blue_3_channel_scale)
-#	terrain_shader.set_shader_param("blue_3_diffuse", blue_3_channel_diffuse)
-#	terrain_shader.set_shader_param("blue_3_normal", blue_3_channel_normal)
-#	terrain_shader.set_shader_param("blue_3_roughness", blue_3_channel_roughness)
-#	terrain_shader.set_shader_param("blue_3_ao", blue_3_channel_ao)
-#
-#	terrain_shader.set_shader_param("alpha_3_scale", alpha_3_channel_scale)
-#	terrain_shader.set_shader_param("alpha_3_diffuse", alpha_3_channel_diffuse)
-#	terrain_shader.set_shader_param("alpha_3_normal", alpha_3_channel_normal)
-#	terrain_shader.set_shader_param("alpha_3_roughness", alpha_3_channel_roughness)
-#	terrain_shader.set_shader_param("alpha_3_ao", alpha_3_channel_ao)
+	terrain_shader.set_shader_param("alpha_scale", alpha_channel_scale)
+	terrain_shader.set_shader_param("alpha_diffuse", alpha_channel_diffuse)
+	terrain_shader.set_shader_param("alpha_normal", alpha_channel_normal)
+	terrain_shader.set_shader_param("alpha_roughness", alpha_channel_roughness)
+	terrain_shader.set_shader_param("alpha_ao", alpha_channel_ao)
 
 
 func process_images():
