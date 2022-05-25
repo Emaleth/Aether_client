8cf370072fd0a1fd86979287ceb15574c4abfe22
Emaleth <Emaleth@protonmail.com>
2022-05-25 22:41:43 +0200

    Changes to be committed: 	modified:   resources/materials/terrain.tres 	modified:   source/UI/layers/dashboard/panels/currency/CurrencyContainer.gd 	modified:   source/UI/layers/dashboard/panels/currency/CurrencyContainer.tscn 	modified:   source/UI/layers/dashboard/panels/item_slot/ItemSlot.tscn 	modified:   source/UI/layers/dashboard/panels/stats/StatsContainer.gd 	modified:   source/UI/layers/dashboard/panels/stats/StatsContainer.tscn 	modified:   source/world/World.tscn

--- source/UI/layers/dashboard/panels/currency/CurrencyContainer.gd
+++ source/UI/layers/dashboard/panels/currency/CurrencyContainer.gd
@@ -1,5 +1,8 @@
 extends PanelContainer
 
 
+onready var gold_label := $HBoxContainer/GoldLabel
+
+
 func configure(_data):
-	print(_data)
+	gold_label.text = "Gold: %s" % _data["gold"]
--- source/UI/layers/dashboard/panels/stats/StatsContainer.gd
+++ source/UI/layers/dashboard/panels/stats/StatsContainer.gd
@@ -1,5 +1,16 @@
 extends PanelContainer
 
 
+onready var dex_box := $VBoxContainer/dex
+onready var int_box := $VBoxContainer/int
+onready var sta_box := $VBoxContainer/sta
+onready var str_box := $VBoxContainer/str
+onready var wis_box := $VBoxContainer/wis
+
+
 func configure(_data):
-	print(_data)
+	dex_box.get_node("Label").text ="Dexterity: %s" % _data["dex"]
+	int_box.get_node("Label").text ="Intelligence: %s" % _data["int"]
+	sta_box.get_node("Label").text ="Stamina: %s" % _data["sta"]
+	str_box.get_node("Label").text ="Strenght: %s" % _data["str"]
+	wis_box.get_node("Label").text ="Wisdom: %s" % _data["wis"]
